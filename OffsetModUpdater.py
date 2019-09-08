85b0eeb29f5a451e4bf18c40aa4d3c6ad7e1605c
Genwald <sethv27@gmail.com>
2019-09-08 05:00:53 -0500

    Add files via upload

---
+++ OffsetModUpdater.py
@@ -0,0 +1,39 @@
+import os, csv, sys
+from shutil import copyfile
+import zstandard as zstd
+
+# barrowed from https://github.com/Birdwards/SmashPad
+def decomp(input_name, output_name):
+    o = open(output_name, 'w+b')
+    i = open(input_name, 'rb').read()
+    dctx = zstd.ZstdDecompressor()
+    for chunk in dctx.read_to_iter(i):
+        o.write(chunk)
+    n = o.tell()
+    o.close()
+    print(output_name + "\nsuccessfully decompressed to " + hex(n) + " bytes!")
+
+
+if len(sys.argv) < 2:
+    sys.exit("Please provide a mod directory and arc version as arguments")
+modDir = sys.argv[1]
+arcVersion = sys.argv[2]
+outputBase = os.path.join("output", os.path.basename(modDir))
+
+with open("Offsets" + arcVersion + ".txt", "r") as offsetsFile:
+    for root, dirs, files in os.walk(modDir, False):
+        offsetsFile.seek(0)
+        for line in offsetsFile:
+            fileInfo = line.split(",")
+            offset = fileInfo[1]
+            arcPath = fileInfo[0]
+            for fileName in files:
+                if offset in fileName:
+                    filePath = os.path.normpath(os.path.join(root, fileName))
+                    outPath = os.path.normpath(os.path.join(outputBase, arcPath))
+                    os.makedirs(os.path.dirname(outPath), 0o777, True)
+                    try:
+                        decomp(filePath, outPath)
+                    # if the file can't be decompressed, copy instead
+                    except zstd.ZstdError:
+                        copyfile(filePath, outPath)
